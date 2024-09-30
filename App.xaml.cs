6f4a6843b2d3bc4f5a9ed24a02e406077d9694bc
Lucas Isabel <lucasestudo2020@gmail.com>
2024-09-30 19:44:42 -0300

    MAUI CENTRAL APP

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace COMBO_PRINT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace COMBO_PRINT
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
